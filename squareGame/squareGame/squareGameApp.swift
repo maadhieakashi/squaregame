9b1e470f11373d4c6d0e8fcb4b273babfd4d6fec
SAHimeshi 002 <sahimeshi002@NIBMADMIN04s-iMac.local>
2025-05-16 11:10:21 +0530

    first commit

---
+++ squareGame/squareGame/squareGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  squareGameApp.swift
+//  squareGame
+//
+//  Created by SAHimeshi 002 on 2025-05-04.
+//
+
+import SwiftUI
+
+@main
+struct squareGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ squareGame/squareGameTests/squareGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  squareGameTests.swift
+//  squareGameTests
+//
+//  Created by SAHimeshi 002 on 2025-05-04.
+//
+
+import Testing
+@testable import squareGame
+
+struct squareGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
